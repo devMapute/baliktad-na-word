eef7722aa870e716e48dff293b952cb54faf3a0f
devMapute <ammapute1@up.edu.ph>
2025-04-03 19:00:15 +0800

    Enhance client-server interaction by allowing user input for server IP address and refactor word transformation function

--- client.c
+++ client.c
@@ -23,8 +23,13 @@ int main(void)
     // Set port and IP the same as server-side:
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(2000);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    char ip_address[100];
+    printf("Enter server IP address: ");
+    fgets(ip_address, sizeof(ip_address), stdin);
+    ip_address[strcspn(ip_address, "\n")] = 0; // Remove trailing newline
+    server_addr.sin_addr.s_addr = inet_addr(ip_address); // Get user input for IP address
     
+    // server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     // Send a connection request to the server, which is waiting at accept():
     if(connect(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
         printf("Unable to connect\n");
@@ -57,30 +62,11 @@ int main(void)
         }
         
         printf("Server's response: %s\n",server_message);
-
+        
+        // Clean buffers:
+        memset(server_message,'\0',sizeof(server_message));
         memset(client_message,'\0',sizeof(client_message));
     }
     
-    // // Get input from the user:
-    // printf("Enter message: ");
-    // fgets(client_message,2000,stdin);
-    
-    // // Send the message to server:
-    // if(send(socket_desc, client_message, strlen(client_message), 0) < 0){
-    //     printf("Unable to send message\n");
-    //     return -1;
-    // }
-    
-    // // Receive the server's response:
-    // if(recv(socket_desc, server_message, sizeof(server_message), 0) < 0){
-    //     printf("Error while receiving server's msg\n");
-    //     return -1;
-    // }
-    
-    // printf("Server's response: %s\n",server_message);
-    
-    // // Close the socket:
-    // close(socket_desc);
-    
-    // return 0;
+
 }
--- server.c
+++ server.c
@@ -18,8 +18,7 @@ int isVowel(char c){
 }
 
 int isOneSyllable(char *word, char *output){
-    char words[10][10] = {"ng", "ang", "hays", "mga"};
-
+    char words[10][10] = {"ang", "hays", "mga", ""};
     for (int i = 0; i < 4; i++){
         if (strcmp(word, words[i]) == 0){
             return 1;
@@ -29,26 +28,26 @@ int isOneSyllable(char *word, char *output){
 
 }
 
-void transformWord(char *word, char *output) {
+void tadbalikWord(char *word, char *output) {
     int len = strlen(word);
     int ptr = len - 1;
-    if (len <= 1) {
-        strcpy(output, word); // For single letters, just copy as is
+    if (len <= 2) {
+        printf("Word with 2 or fewer letters detected: %s\n", word);
+        strcpy(output, word); // For words with 2 or fewer letters, just copy as is
+        printf("Translated: %s\n", output);
         return;
     } else if (len <= 4) {
         int is_one_syllable = isOneSyllable(word, output);
         if (is_one_syllable) {
             printf("One-syllable word detected: %s\n", word);
             strcpy(output, word); // For one-syllable words, just copy as is
-            printf("Transformed: %s\n", output);
+            printf("Translated: %s\n", output);
             return;
         }
     } 
     
-    
     char first_part[2000] = {0};
     char last_part[2000] = {0};
-    // int split_point = -1;
 
     if (isVowel(word[len-1])){ // Last letter is vowel
         if (isVowel(word[len-2])){ // Last two letters are vowels
@@ -59,15 +58,25 @@ void transformWord(char *word, char *output) {
             }
             last_part[len-1] = '\0';
         } else{ // Last letter is vowel, second to last is consonant
-            first_part[0] = word[len-2]; // Get second to the last letter
-            first_part[1] = word[len-1]; // Get the last letter
-            first_part[2] = '\0';
-            for (int i = 0; i < len-2; i++) { // Copy the rest of the word
-                last_part[i] = word[i];
-            }
-            last_part[len-2] = '\0';
+            if (word[len-3] == 'n' && word[len-2] == 'g'){
+                first_part[0] = word[len-3]; // Get the third to the last letter
+                first_part[1] = word[len-2]; // Get the second to the last letter
+                first_part[2] = word[len-1]; // Get the last letter
+                first_part[3] = '\0';
+                for (int i = 0; i < len-3; i++) { // Copy the rest of the word
+                    last_part[i] = word[i];
+                }
+                last_part[len-2] = '\0';
                 
-            // }
+            } else {                
+                first_part[0] = word[len-2]; // Get second to the last letter
+                first_part[1] = word[len-1]; // Get the last letter
+                first_part[2] = '\0';
+                for (int i = 0; i < len-2; i++) { // Copy the rest of the word
+                    last_part[i] = word[i];
+                }
+                last_part[len-2] = '\0';
+            }
         }
     } else { // Last letter is a consonant
         if (isVowel(word[len-2])){ // Last letter is consonant, second to last is vowel
@@ -81,6 +90,17 @@ void transformWord(char *word, char *output) {
                 last_part[len-2] = '\0';
             }
             else { // Last letter is consonant, second to last is vowel, third to last is consonant
+                if (word[len-4] == 'n' && word[len-3] == 'g'){
+                    first_part[0] = word[len-4]; // Get the fourth to last letter
+                    first_part[1] = word[len-3]; // Get the third to last letter
+                    first_part[2] = word[len-2]; // Get the second to last letter
+                    first_part[3] = word[len-1]; // Get the last letter
+                    first_part[4] = '\0';
+                    for (int i = 0; i < len-4; i++) { // Copy the rest of the word
+                        last_part[i] = word[i];
+                    }
+                    last_part[len-3] = '\0';
+                } else {
                 first_part[0] = word[len-3]; // Get the third to last letter
                 first_part[1] = word[len-2]; // Get the second to last letter
                 first_part[2] = word[len-1]; // Get the last letter
@@ -89,6 +109,7 @@ void transformWord(char *word, char *output) {
                     last_part[i] = word[i];
                 }
                 last_part[len-1] = '\0';
+            }
 
             }
 
@@ -112,17 +133,13 @@ void transformWord(char *word, char *output) {
     strcat(output, last_part);
     
     
-    // Print the transformed word
-    printf("Transformed: %s\n", output);
+    // Print the translated word
+    printf("Translated: %s\n", output);
 }
 
 int main(void)
 {
     int socket_desc, client_sock, client_size;
-    // int string_len;
-    // int consonant_flag;
-    // int i,j, ptr;
-    // char c;
     struct sockaddr_in server_addr, client_addr;
     char server_message[2000], client_message[2000];
     
@@ -190,49 +207,9 @@ int main(void)
         }
         printf("Msg from client: %s\n", client_message);
 
-        transformWord(client_message, server_message);
+        tadbalikWord(client_message, server_message);
 
-        // string_len = strlen(client_message);
-        // printf("String length: %d\n", string_len);
-        // ptr = string_len-1;
-        // // Last letter is consonant
-        // consonant_flag = !isVowel(client_message[ptr]);
-        // for (i = string_len-1; i > 0; i--){
-        //     if (consonant_flag){
-        //         for ( j = i-2; j <= ptr; j++){
-        //             printf("%c", client_message[j]);
-        //         }
-        //         consonant_flag = 0;
-        //         printf("\n");
-        //         ptr = i-2;
-        //         i=i-2;
-
-        //         for (i = 0; i < ptr; i ++){
-        //             printf("%c",client_message[i]);
-        //         }
-        //         printf("\n");
-        //         break;
-        //     } else{
-        //         for ( j = i-1; j <= ptr; j++){
-        //             printf("%c", client_message[j]);
-        //         }
-        //         printf("\n");
-        //         ptr = i-1;
-        //         i--;
-                
-        //         for (i = 0; i < ptr; i ++){
-        //             printf("%c",client_message[i]);
-        //         }
-        //         printf("\n");
-        //         break;
-        //     }
-
-        // }
-
-        
-
-
-        strcpy(server_message, "This is the server's message.");
+        // strcpy(server_message, "This is the server's message.");
         if (send(client_sock, server_message, strlen(server_message), 0) < 0){
             printf("Can't send\n");
             return -1;
@@ -243,23 +220,4 @@ int main(void)
         memset(client_message, '\0', sizeof(client_message));       
 
     }
-    // if (recv(client_sock, client_message, sizeof(client_message), 0) < 0){
-    //     printf("Couldn't receive\n");
-    //     return -1;
-    // }
-    // printf("Msg from client: %s\n", client_message);
-    
-    // // Respond to client:
-    // strcpy(server_message, "This is the server's message.");
-    
-    // if (send(client_sock, server_message, strlen(server_message), 0) < 0){
-    //     printf("Can't send\n");
-    //     return -1;
-    // }
-    
-    // // Closing the socket:
-    // close(client_sock);
-    // close(socket_desc);
-    
-    // return 0;
 }
